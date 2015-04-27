47dcf693d6f0ad659ab39032cb7abb8e61bc4298
Aster <aster@vers.site>
2015-04-27 09:14:54 +0000

    📦 Refresh this book's table of contents

--- projects/book/docs/.vuepress/config.ts
+++ projects/book/docs/.vuepress/config.ts
@@ -10,7 +10,7 @@ import * as path from "path";
 const locales: SiteLocaleConfig = {
     '/cn/': {
         lang: 'zh-CN',
-        title: 'Valkyrie 语言教程',
+        title: '注意力训练手册',
     },
     '/en/': {
         lang: 'en-US',
@@ -18,235 +18,42 @@ const locales: SiteLocaleConfig = {
     }
 };
 
-const sidebar_en: SidebarConfigArray = [
-    {
-        text: 'Literals',
-        collapsible: false,
-        children: [
-            {
-                text: "Overview",
-                link: "/en/literal/readme.md",
-            },
-            {
-                text: "Number Literal",
-                link: "/en/literal/number.md",
-            },
-            {
-                text: "String Formatted",
-                link: "/en/literal/string.md",
-            },
-            {
-                text: "String Templated",
-                link: "/en/literal/template.md",
-            },
-        ]
-    },
-    {
-        text: 'Collections',
-        collapsible: false,
-        children: [
-            {
-                text: "Overview",
-                link: "/en/collection/readme.md",
-            },
-            {
-                text: "List",
-                link: "/en/collection/list.md",
-            },
-            {
-                text: "Dict",
-                link: "/en/collection/dict.md",
-            },
-            {
-                text: "Map",
-                link: "/en/collection/map.md",
-            },
-            {
-                text: "Set",
-                link: "/en/collection/set.md",
-            },
-        ]
-    },
-    {
-        text: 'Definitions',
-        collapsible: false,
-        children: [
-            {
-                text: "Overview",
-                link: "/en/definition/readme.md",
-            },
-            {
-                text: "Let Bind",
-                link: "/en/definition/let-binding.md",
-            },
-            {
-                text: "Micro Process",
-                link: "/en/definition/def-micro.md",
-            },
-            {
-                text: "Macro Process",
-                link: "/en/definition/def-macro.md",
-            },
-            {
-                text: "Types",
-                link: "/en/definition/typing.md",
-            },
-            {
-                text: "Structures",
-                link: "/en/definition/structure.md",
-            },
-            {
-                text: "Inheritance",
-                link: "/en/definition/subtyping.md",
-            },
-            {
-                text: "Interfaces",
-                link: "/en/definition/interface.md",
-            },
-            {
-                text: "Association Type",
-                link: "/en/definition/associated.md",
-            },
-            {
-                text: "Enumerations",
-                link: "/en/definition/enumerate.md",
-            },
-            {
-                text: "Flags",
-                link: "/en/definition/flags.md",
-            },
-            {
-                text: "Unions",
-                link: "/en/definition/union.md",
-            },
-            {
-                text: "Unites",
-                link: "/en/definition/disjoint-union.md",
-            },
-        ]
-    },
-    {
-        text: 'Invokes',
-        collapsible: false,
-        children: [
-            {
-                text: "Overview",
-                link: "/en/invoke/readme.md",
-            },
-            {
-                text: "Function Call",
-                link: "/en/invoke/apply.md",
-            },
-            {
-                text: "Function Dot Call",
-                link: "/en/invoke/apply-dot.md",
-            },
-            {
-                text: "Index Call",
-                link: "/en/invoke/subscript.md",
-            },
-            {
-                text: "Slice Call",
-                link: "/en/invoke/slice.md",
-            },
-            {
-                text: "Lambda Call",
-                link: "/en/invoke/lambda.md",
-            },
-            {
-                text: "Lambda Dot Call",
-                link: "/en/invoke/lambda-dot.md",
-            }
-        ]
-    },
-    {
-        text: 'Control Flow',
-        collapsible: false,
-        target: "/en/control/readme.md",
-        children: [
-            {
-                text: "Overview",
-                link: "/en/control/readme.md"
-            },
-            {
-                text: "If Statement",
-                link: "/en/control/jump-if.md"
-            },
-            {
-                text: "Switch Statement",
-                link: "/en/control/jump-switch.md"
-            },
-            {
-                text: "Non Locally Return",
-                link: "/en/control/jump-control.md"
-            },
-            {
-                text: "While Loop",
-                link: "/en/control/loop-while.md"
-            },
-            {
-                text: "For Loop",
-                link: "/en/control/loop-for.md"
-            },
-            {
-                text: "Auto Handler",
-                link: "/en/control/handle-try.md"
-            },
-            {
-                text: "Effect Handler",
-                link: "/en/control/handle-catch.md"
-            },
-            {
-                text: "Typing Handler",
-                link: "/en/control/handle-match.md"
-            },
-        ]
-    },
-    {
-        text: 'Advance',
-        collapsible: false,
-        children: [
-            {
-                text: "Overview",
-                link: "/en/advance/readme.md",
-            },
-            {
-                text: "Pattern Match",
-                link: "/en/advance/pattern-match.md",
-            },
-            {
-                text: "Extractor",
-                link: "/en/advance/extractor.md",
-            },
-            {
-                text: "Builder",
-                link: "/en/advance/builder.md",
-            },
-            {
-                text: "Logger",
-                link: "/en/advance/logger.md",
-            },
-            {
-                text: "Effect System",
-                link: "/en/advance/effect-system.md",
-            },
-        ]
-    },
-    {
-        text: 'Appendix',
-        collapsible: false,
-        children: [
-            {
-                text: "Keywords Summary",
-                link: "/en/appendix/readme.md",
-            },
-            {
-                text: "Rust User Guide",
-                link: "/en/appendix/for-rust.md",
-            },
-        ]
-    }
-]
+function build_sidebar(lang: string): SidebarConfigArray {
+    return [
+        {
+            text: 'π 家族',
+            collapsible: true,
+            children: [
+                {
+                    text: "4k + i 型逼近",
+                    link: `/${lang}/π-family/4k+i.md`,
+                },
+                {
+                    text: "Number Literal",
+                    link: "/en/literal/number.md",
+                },
+                {
+                    text: "String Formatted",
+                    link: "/en/literal/string.md",
+                },
+                {
+                    text: "String Templated",
+                    link: "/en/literal/template.md",
+                },
+            ]
+        },
+        {
+            text: 'e 家族',
+            collapsible: false,
+            children: []
+        },
+        {
+            text: 'Appendix',
+            collapsible: false,
+            children: []
+        }
+    ]
+}
 
 const theme = defaultTheme(
     {
@@ -262,7 +69,7 @@ const theme = defaultTheme(
                 selectLanguageName: "简体中文",
                 editLinkText: '在 GitHub 上编辑此页',
                 sidebar: {
-                    "/cn/": sidebar_en,
+                    "/cn/": build_sidebar("cn"),
                 },
             },
             '/en/': {
@@ -270,7 +77,7 @@ const theme = defaultTheme(
                 selectLanguageName: 'English',
                 editLinkText: 'Edit this page on GitHub',
                 sidebar: {
-                    "/en/": sidebar_en
+                    "/en/": build_sidebar("en"),
                 },
                 navbar: []
             },
--- projects/prover/src/lib.rs
+++ projects/prover/src/lib.rs
@@ -1,15 +1,2 @@
-use std::path::Path;
-use yggdrasil_shared::{codegen::BuildRust, FileCache};
-
-pub fn build<I, O>(input: I, output: O, config: BuildRust) -> std::io::Result<()>
-where
-    I: AsRef<Path>,
-    O: AsRef<Path>,
-{
-    let mut cache = FileCache::default();
-    let file = cache.load_local(input)?;
-    config.generate(file, &mut cache, output).option(|e| {
-        e.as_report().eprint(&cache).ok();
-    });
-    Ok(())
-}
+mod about_pi;
+mod about_e;
