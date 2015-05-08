a81996dd9427c2f6fe22a4b07a62c63677d414eb
Aster <aster@vers.site>
2015-05-08 18:10:54 +0000

    🧪 Evaluate Approx4K(stage 2) for range 1-8

--- projects/prover/src/about_pi/approx_4k_i/main.m
+++ projects/prover/src/about_pi/approx_4k_i/main.m
@@ -1,26 +1,14 @@
 (* ::Package:: *)
 
-patterns=Import["4k+1.json","RawJSON"]
-exprs=Flatten[Table[
-Inactive[Sum][1/Product[4k+i,{i,#}],{k,j,Infinity}],
-{j,0,5}
-]&/@patterns];
-searches=Denominator/@Convergents@ContinuedFraction[Pi,20];
-shift4[expr_]:=Block[
-	{coeff,normed,denom},
-	coeff=CoefficientList[Activate@expr,Pi];
-	normed=Denominator@Last@coeff;
-	denom=Denominator@First[normed*coeff];
-	If[MemberQ[searches,denom],normed*expr,Nothing]
-]
-Collect[outs//Activate,Pi]
+(* ::Section:: *)
+(*Find All Stage*)
 
 
 filterMixed[params_List] := Block[
   {symbols, expr, syms},
   (symbols = {Log, Pi, EulerGamma};
      expr = ExpandAll@Sum[1/Product[(4 k + i), {i, params}], {k, 0, Infinity}];
-     If[! FreeQ[expr, PolyGamma], Return@Nothing];)
+     If[! FreeQ[expr, PolyGamma], Return@Nothing];);
     syms = Sum[Boole@FreeQ[expr, term], {term, symbols}];
   If[syms < 2, Return@Nothing];
   params -> expr
@@ -28,8 +16,25 @@ filterMixed[params_List] := Block[
 
 
 SetDirectory@NotebookDirectory[];
-params=Select[Subsets[Range@20,{2,20}],Min[#]<5&];
+params=Select[Subsets[Range@24,{2,12}],Min[#]<5&];
 Length[params]
 data=ParallelMap[filterMixed,params];
 Length[data]
-Export["data.m",data]
+Export["4k+1.m",data]
+
+
+(* ::Input:: *)
+(*patterns = Import["4k+1.json", "RawJSON"]*)
+(*exprs = Flatten[Table[*)
+(*            Inactive[Sum][1/Product[4 k + i, {i, #}], {k, j, Infinity}],*)
+(*            {j, 0, 5}*)
+(*            ] & /@ patterns];*)
+(*searches = Denominator /@ Convergents@ContinuedFraction[Pi, 20];*)
+(*shift4[expr_] := Block[*)
+(*    	{coeff, normed, denom},*)
+(*    	coeff = CoefficientList[Activate@expr, Pi];*)
+(*    	normed = Denominator@Last@coeff;*)
+(*    	denom = Denominator@First[normed*coeff];*)
+(*    	If[MemberQ[searches, denom], normed*expr, Nothing]*)
+(*    ]*)
+(*Collect[outs // Activate, Pi]*)
